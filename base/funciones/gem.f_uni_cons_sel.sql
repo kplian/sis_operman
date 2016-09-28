fbe96d93dae590918f59591a73454ac152cb56f9
Rodrigo Chumacero <rodrigo@kplian.com>
2016-09-28 11:53:36 -0400

    adecuacion filtro

--- base/funciones/gem.f_uni_cons_sel.sql
+++ base/funciones/gem.f_uni_cons_sel.sql
@@ -1286,7 +1286,7 @@ BEGIN
       	v_consulta = v_consulta || v_cond;
       end if;
       v_consulta:=v_consulta||' order by ' ||v_parametros.ordenacion|| ' ' || v_parametros.dir_ordenacion || ' limit ' || v_parametros.cantidad || ' offset ' || v_parametros.puntero;
-      raise notice '%',v_consulta;
+      raise exception '%',v_consulta;
       --Devuelve la respuesta
       return v_consulta;
             
